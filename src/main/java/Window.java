194bba844fa025539d4f1dc3e33186fb9258164c
Daniel <74980223+MustafarajDaniel@users.noreply.github.com>
2023-06-26 18:39:00 +0200

    Change Colors

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,25 +1,86 @@
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Color;
 
 
+/**
+ * The Theme class represents a collection of colors used for application.
+ */
+class Theme {
+    /**
+     * The color for the big block.
+     */
+    static Color bigBlock = Color.decode("#001C30");
+
+    /**
+     * The color for a regular block.
+     */
+    static Color block = Color.decode("#176B87");
+
+    /**
+     * The color for a selected block.
+     */
+    static Color blockSelected = Color.decode("#64CCC5");
+
+    /**
+     * The color for the border of a block.
+     */
+    static Color blockBorder = Color.decode("#FFFFFF");
+
+    /**
+     * The color for an end point to win the game.
+     */
+    static Color endPoint = Color.RED;
+
+    /**
+     * The background color.
+     */
+    static Color background = Color.decode("#DAFFFB");
+
+    /**
+     * The color for a menu.
+     */
+    static Color menu = Color.WHITE;
+
+    /**
+     * The color for menu text.
+     */
+    static Color menuText = Color.BLACK;
+
+    /**
+     * The color for menu text whene the mouse is on hover.
+     */
+    static Color menuTextHover = Color.ORANGE;
+}
+
+
+/**
+ * This class represents a customized button that extends the JButton class.
+ * It provides a default style for buttons with specific colors.
+ */
 class Button extends JButton {
+
+    /**
+     * Constructs a Button with the specified label.
+     * @param str The label text for the button.
+     */
     Button(String str) {
         super(str);
-        setBackground(Color.BLACK);
-        setForeground(Color.WHITE);
+        setBackground(Theme.menu);
+        setForeground(Theme.menuText);
         setBorderPainted(false);
         setFocusable(false);
 
         addMouseListener(new MouseAdapter() {
             public void mouseEntered(MouseEvent e) {
-                setForeground(Color.ORANGE);
+                setForeground(Theme.menuTextHover);
             }
         
             public void mouseExited(MouseEvent e) {
-                setForeground(Color.WHITE);
+                setForeground(Theme.menuText);
             }
         });
     }
@@ -28,9 +89,20 @@ class Button extends JButton {
 public class Window extends JFrame  {
 
     final static String winName = "Klotski";
-    final static public int BLOCK_SIZE = 120;
+    final static public int BLOCK_SIZE = 130;
     final static public int BOARD_WIDTH = Board.WIDTH * BLOCK_SIZE;
     final static public int BOARD_HEIGHT = Board.HEIGHT * BLOCK_SIZE + 5;
+    final static private String infoTitle = "This project is the Klotsky game developed by 4 students.";
+    final static private String infoGameRules = "GOAL:\n" +
+    "Among the blocks, there is a special one (the 2x2 red) which\n" +
+    "must be moved to a special area, where there is the red line.\n" +
+    "RULES:\n" +
+    "The player may only slide blocks horizontally and vertically,\n" +
+    "cannot bring pieces outside the board and, for that, when you\n" +
+    "leave the board with the mouse the action will be cancelled.\n" +
+    "HELP:\n" +
+    "With the button \"Best move\" the best move will be made,\n" +
+    "with the button \"Solve all\" all the puzzle will be solved";
 
     protected JPanel menu;
     private JMenuBar mainBar;
@@ -58,6 +130,8 @@ public class Window extends JFrame  {
 
         String[] levels = { "Level 1", "Level 2", "Level 3", "Level 4" };
         JComboBox<String>  selectLevel = new JComboBox<String>(levels);
+        // selectLevel.setBackground(Theme.menu);
+        // selectLevel.setForeground(Theme.menuText);
         menu.putClientProperty("level_selection", selectLevel);
         selectLevel.setPreferredSize(new Dimension(100, 50));
 
@@ -75,7 +149,7 @@ public class Window extends JFrame  {
 
         // Create BOARD
         board_view = new JPanel(null);
-        board_view.setBackground(Color.GRAY);
+        board_view.setBackground(Theme.background);
 
         JButton b = new JButton("TEST");
         b.setBackground(Color.ORANGE);
@@ -89,6 +163,18 @@ public class Window extends JFrame  {
         
         JMenu fileMenu = new JMenu("Settings");
         menuBar.putClientProperty("Settings", fileMenu);
+
+        fileMenu.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                fileMenu.setForeground(Theme.menuTextHover);
+            }
+        
+            @Override
+            public void mouseExited(MouseEvent e) {
+                fileMenu.setForeground(Theme.menuText);
+            }
+        });
 
         JMenu levelMenu = new JMenu("Levels");
         menuBar.putClientProperty("Levels", levelMenu);
@@ -181,7 +267,7 @@ public class Window extends JFrame  {
         pieces_view = new JPanel[pieces.length];
         
         JButton exit = new JButton();
-        exit.setBackground(Color.RED);
+        exit.setBackground(Theme.endPoint);
         exit.setEnabled(false); // Set the button as disabled
         exit.setBounds(BLOCK_SIZE, BOARD_HEIGHT - 5, BLOCK_SIZE * 2, 5);
         board_view.add(exit);
@@ -190,12 +276,12 @@ public class Window extends JFrame  {
             int[] prop = pieces[i].getProperties();
             pieces_view[i] = new JPanel();
             pieces_view[i].setBounds(prop[0] * BLOCK_SIZE, prop[1] * BLOCK_SIZE, prop[2] * BLOCK_SIZE, prop[3] * BLOCK_SIZE);
-            pieces_view[i].setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.GRAY));
+            pieces_view[i].setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Theme.blockBorder));
             if(board.isSpecial(i)) {
-                pieces_view[i].setBackground(Color.RED);
+                pieces_view[i].setBackground(Theme.bigBlock);
             }
             else {
-                pieces_view[i].setBackground(Color.ORANGE);
+                pieces_view[i].setBackground(Theme.block);
             }
             board_view.add(pieces_view[i]);
         }
@@ -225,15 +311,15 @@ public class Window extends JFrame  {
     }
 
     public void pressedPiece(int index) {
-        pieces_view[index].setBackground(Color.BLUE);
+        pieces_view[index].setBackground(Theme.blockSelected);
     }
 
     public void releasedPiece(int index, boolean isSpecial) {
         if(isSpecial) {
-            pieces_view[index].setBackground(Color.RED);
+            pieces_view[index].setBackground(Theme.bigBlock);
         }
         else {
-            pieces_view[index].setBackground(Color.ORANGE);
+            pieces_view[index].setBackground(Theme.block);
         }
     }
 
@@ -269,17 +355,8 @@ public class Window extends JFrame  {
 
     private void showInfo() {
         JOptionPane.showMessageDialog(this,
-                "GOAL:\n" +
-                        "Among the blocks, there is a special one (the 2x2 red) which\n" +
-                        "must be moved to a special area, where there is the red line.\n" +
-                        "RULES:\n" +
-                        "The player may only slide blocks horizontally and vertically,\n" +
-                        "cannot bring pieces outside the board and, for that, when you\n" +
-                        "leave the board with the mouse the action will be cancelled.\n" +
-                        "HELP:\n" +
-                        "With the button \"Best move\" the best move will be made,\n" +
-                        "with the button \"Solve all\" all the puzzle will be solved",
-                "This project is the Klotsky game developed by 4 students.",
+                infoGameRules,
+                infoTitle,
                 JOptionPane.INFORMATION_MESSAGE);
     }
 }
