ba2630c5e3b6364c2bc48f9b4f1835fb5243ac1f
Nicola Ramoso <nicolaramoso@gmail.com>
2023-06-30 14:37:43 +0200

    Javadocs MovesLog

--- src/main/java/MovesLog.java
+++ src/main/java/MovesLog.java
@@ -1,42 +1,63 @@
 import java.util.Stack;
 
+/**
+ * Stores the number of moves that are performed during gameplay
+ */
 public class MovesLog {
     private Stack<Move> moves_log;
     private Stack<Move> saved_moves_log;
     private int step;
 
+    /**
+     * Resets the counter
+     */
     public MovesLog() {
         resetLog();
     }
 
+    /**
+     * Resets the counter and its related data structures
+     */
     public void resetLog() {
         moves_log = new Stack<>();
         step = 0;
     }
 
-    // push a move on the moves_log stack
+    /** Push a new move on the moves_log stack
+     * @param piece_index index of the moving piece
+     * @param move_direction direction of movement of the moving piece
+     */
+    //
     public void pushMove(int piece_index, int move_direction) {
         moves_log.push(new Move(step, piece_index, move_direction));
         step++;
     }
 
-    // pop a move from the moves_log stack and return it
+    /** Pop a move from the moves_log stack and return it
+     * @return
+     */
     public Move popMove() {
         step--;
         return moves_log.pop();
     }
 
-    // check if moves_log is empty or null
+    /** Check if moves_log is empty or null
+     * @return true if empty
+     */
     public boolean isEmpty() {
         return moves_log == null || moves_log.isEmpty();
     }
 
-    // copy moves_log to saved_moves_log
+    /**
+     * Copy moves_log to saved_moves_log
+     */
     public void saveLog() {
         saved_moves_log = (Stack<Move>) moves_log.clone();
     }
 
-    // copy saved_moves_log to moves_log
+    /**
+     * Copy saved_moves_log to moves_log
+     */
     public void loadLog() {
         if(!(saved_moves_log == null || saved_moves_log.isEmpty())) {
             moves_log = (Stack<Move>) saved_moves_log.clone();
@@ -44,6 +65,9 @@ public class MovesLog {
         }
     }
 
+    /** Gets the current step
+     * @return int step
+     */
     public int getStep() {
         return step;
     }
