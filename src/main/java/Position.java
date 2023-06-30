2408a8c6474d56b4b82071cfda3bd8e1f089e2ec
Nicola Ramoso <nicolaramoso@gmail.com>
2023-06-30 14:06:26 +0200

    Javadocs Position

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,33 +1,52 @@
 import java.awt.*;
 
+/**
+ * Implements the abstracion of each square on the checkerboard
+ */
 public class Position{
     int x;
     int y;
     final int MIN_MOUSE_DRAG = 3;
 
-    // initialize x and y checking if they are inside the board (in pixel)
+    /** initialize x and y checking if they are inside the board (in pixel)
+     * @param x x value
+     * @param y y value
+     */
+
     public Position(int x, int y) {
         if(x < 0 || y < 0 || x > Window.BOARD_WIDTH || y > Window.BOARD_HEIGHT)
             throw new IllegalArgumentException("Position outside the board");
         this.x = x;
         this.y = y;
     }
-    // constructor with Point (mainly for the mouse getPoint() method that returns a point)
+
+    /** constructor with Point (mainly for the mouse getPoint() method that returns a point)
+     * @param point
+     */
     public Position(Point point) {
         this(point.x, point.y);
     }
 
-    // x and y distance
+    /** Returns the distance on the x coordinates between a Position passed as argument and the x value of the Position
+     * @param final_pos Position
+     * @return distance
+     */
     public int xDistance(Position final_pos) {
         return final_pos.x - x;
     }
+
+    /** Returns the distance on the y coordinates between a Position passed as argument and the y value of the Position
+     * @param final_pos Position
+     * @return distance
+     */
     public int yDistance(Position final_pos) {
         return final_pos.y - y;
     }
 
-    // return the cardinal direction of the vector this -> final_pos
-    // approximate to:
-    // up=2, right=1, down=0, left=3
+    /** return the cardinal direction of the vector this -> final_pos approximate to: up=2, right=1, down=0, left=3
+     * @param final_pos
+     * @return
+     */
     public int direction(Position final_pos) {
         int dx = xDistance(final_pos);
         int dy = yDistance(final_pos);
@@ -45,20 +64,31 @@ public class Position{
                 return 2;   //up
     }
 
+    /** Checks if the current Position is the same as the one passed as argument
+     * @param pos Position to compare
+     * @return true if the positions are the same
+     */
     public boolean equals(Position pos) {
         return xDistance(pos)==0 && yDistance(pos)==0;
     }
 
-    // convert the pos from pixels to unit of the board
+    /** Converts the pos from pixels the unit of measurement of the board
+     * @return a Position
+     */
     public Position unitConverter() {
         return new Position(x/Window.BLOCK_SIZE, y/Window.BLOCK_SIZE);
     }
 
-    // convert the pos from unit of the board to pixels
+    /** Converts the pos from the unit of measurement the board to pixels
+     * @return Position
+     */
     public Position pixelConverter() {
         return new Position(x*Window.BLOCK_SIZE, y*Window.BLOCK_SIZE);
     }
 
+    /** Returns the coordinates of the position to a String
+     * @return a String of coordinates
+     */
     @Override
     public String toString() {
         return "[" + Integer.toString(x) + "," + Integer.toString(y) + "]";
