8ff7b95244e25bd18b89ebcdc0862a75d7354c18
maxino <maxs.campagnaro@gmail.com>
2023-06-30 16:45:26 +0200

    GameTest finished

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
@@ -8,7 +11,20 @@ class GameTest {
     void setBoard() {
         Game game = new Game();
         int level_number = 1;
-        Board board = new Board(level_number);
+        Piece[] pieces = new Piece[10];
+        // level 1 pieces
+        pieces[0] = new Piece(1, 0, 2, 2);
+        pieces[1] = new Piece(0, 0, 1, 1);
+        pieces[2] = new Piece(3, 0, 1, 1);
+        pieces[3] = new Piece(0, 2, 1, 2);
+        pieces[4] = new Piece(3, 2, 1, 2);
+        pieces[5] = new Piece(1, 2, 2, 1);
+        pieces[6] = new Piece(0, 1, 1, 1);
+        pieces[7] = new Piece(1, 3, 1, 2);
+        pieces[8] = new Piece(2, 3, 1, 2);
+        pieces[9] = new Piece(3, 1, 1, 1);
+
+        Board board = new Board(pieces);
 
         game.setBoard(level_number);
 
@@ -52,14 +68,22 @@ class GameTest {
     void reset() {
         Game game = new Game();
         game.setBoard(1);
+        Board initial_board = game.getBoard();
 
-        // make some moves to change position of pieces on the board
-        game.makeMoves(10);
+        // set some legal moves to do with the level 1
+        List<Move> moves = new ArrayList<>();
+        moves.add(new Move(1, 3, 2));
+        moves.add(new Move(2, 4, 2));
+        moves.add(new Move(3, 5, 3));
+        moves.add(new Move(4, 1, 1));
+
+        // make moves to change position of pieces on the board
+        game.makeMoves(moves);
 
         // reset the game
         game.reset();
 
-        assertTrue(game.getBoard().equals(new Board(1)));   // the board pieces will be the same as before
+        assertTrue(initial_board.equals(game.getBoard()));   // the board pieces will be the same as before
         assertEquals(0, game.getBoard().getMoves());    // moves will be 0
         assertTrue(game.getLog().isEmpty());    // the log of the moves will be empty
     }
@@ -68,33 +92,63 @@ class GameTest {
     void save_loadState() {
         Game game = new Game();
         game.setBoard(1);
-        int moves_num = 10;
 
-        // make some moves to change position of pieces on the board
-        game.makeMoves(moves_num);
+        // set some legal moves to do with the level 1
+        List<Move> moves = new ArrayList<>();
+        moves.add(new Move(1, 3, 0));
+        moves.add(new Move(2, 4, 0));
+        moves.add(new Move(3, 5, 1));
+        moves.add(new Move(4, 6, 0));
+        moves.add(new Move(5, 6, 1));
+        moves.add(new Move(6, 1, 0));
 
-        Board initial_board = game.getBoard();
+
+        // save the number of moves that will be done
+        int moves_num = moves.size();
+
+        // make moves to change position of pieces on the board
+        game.makeMoves(moves);
+
+        Board temp_board = game.getBoard();
 
         game.saveState("save_test-Game.json");  // the game is saved
         game.reset();                               // resetted
         game.loadState("save_test-Game.json");  // then the previous game is loaded
-        assertTrue(initial_board.equals(game.getBoard()));  // the board will be the same as before
+        assertTrue(temp_board.equals(game.getBoard()));  // the board will be the same as before
         assertEquals(moves_num, game.getBoard().getMoves());    // the moves too
     }
 
     @Test
     void undo() {
-    }
+        Game game = new Game();
+        game.setBoard(1);
 
-    @Test
-    void solveAll() {
-    }
+        // set some legal moves to do with the level 1
+        List<Move> moves = new ArrayList<>();
+        moves.add(new Move(1, 3, 0));
+        moves.add(new Move(2, 4, 0));
+        moves.add(new Move(3, 5, 1));
+        moves.add(new Move(4, 6, 0));
 
-    @Test
-    void bestMove() {
-    }
+        // save the number of moves done
+        int moves_num = moves.size();
 
-    @Test
-    void checkWin() {
+        // make moves to change position of pieces on the board
+        game.makeMoves(moves);
+
+        Board temp_board = game.getBoard();
+
+        // set other 2 moves
+        moves.add(new Move(5, 6, 1));
+        moves.add(new Move(6, 1, 0));
+        // and make them
+        game.makeMoves(moves);
+
+        // return back with 2 undo
+        game.undo();
+        game.undo();
+
+        assertTrue(temp_board.equals(game.getBoard()));  // the board will be the same as before the moves
+        assertEquals(moves_num, game.getBoard().getMoves());    // the number of moves too
     }
 }
